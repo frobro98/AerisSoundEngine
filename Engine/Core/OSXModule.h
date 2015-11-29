9eded43e28750c19e2c9b00dac31961e07d80656
Nathan Blane <nathanblane@Nathans-MacBook-Pro.local>
2015-11-29 13:57:35 -0600

    Adding the correctly configured project, plus osx module header

---
+++ Engine/Core/OSXModule.h
@@ -0,0 +1,13 @@
+//
+//  OSXModule.h
+//  Aeris
+//
+//  Created by Nathan Blane on 11/29/15.
+//  Copyright © 2015 Nathan Blane. All rights reserved.
+//
+
+#ifndef OSXModule_h
+#define OSXModule_h
+
+
+#endif /* OSXModule_h */
